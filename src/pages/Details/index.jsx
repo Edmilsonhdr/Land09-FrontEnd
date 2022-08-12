2226225e2b3a743ec3260e11dbf096019056a637
Edmilson Henrique Dias Rodrigues <92001139+Edmilsonhdr@users.noreply.github.com>
2022-08-12 15:14:00 -0300

    Add files via upload

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from './styles.js';
+
+export function Details(){
+  return(
+    <Container>
+      <h1>Hellow World</h1>
+      <span>Edmilson Henrique Dias Rodrigues</span>
+    </Container>
+    
+  )
+}
